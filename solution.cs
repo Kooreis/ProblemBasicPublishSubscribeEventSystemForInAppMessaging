f8ca145f74e0ba4666a69c220a207e0d26ae9dd3
Kooreis <kooreis.official@gmail.com>
2024-12-29 15:03:22 -0600

    Added a 'subscribe' method to the PubSub class. This method takes an event and a callback function as arguments. If the event does not exist in the 'events' object, it is added with an empty array as its value. The callback function is then pushed into the array of the event.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,7 @@
-class PubSub {
-    constructor() {
-        this.events = {};
+subscribe(event, callback) {
+        if (!this.events.hasOwnProperty(event)) {
+            this.events[event] = [];
+        }
+
+        return this.events[event].push(callback);
     }
-}
